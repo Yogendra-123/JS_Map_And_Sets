87e82a5a900b9ad267bf7593d85a539bc57a8d29
= <=>
2022-12-21 11:02:55 +0530

    strings updated

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 // Q.1
 
-let arr = [2,1,5,1,2,3,5,12]
+let s = 'aaabbexxser'
         let newset = new Set();
-        for(let i=0; i<arr.length; i++){
-        newset.add(arr[i])
+        for(let i=0; i<s.length; i++){
+        newset.add(s[i])
         }
-        console.log(newset)
+    console.log(newset)
 
 
 // Q .2
 
-let s = 'AABBADCCS'
-let map = new Map();
-for(let i=0; i<s.length; i++){
-  if(map.has(s[i])){
-    let value = map.get(s[i]);
-    map.set(s[i], value+1)
-  }
-  else{
-    map.set(s[i],1)
-  }
-}
-for (let [k,v] of map){
-  console.log(k+"="+v)
-}
+// let s = 'AABBADCCS'
+// let map = new Map();
+// for(let i=0; i<s.length; i++){
+//   if(map.has(s[i])){
+//     let value = map.get(s[i]);
+//     map.set(s[i], value+1)
+//   }
+//   else{
+//     map.set(s[i],1)
+//   }
+// }
+// for (let [k,v] of map){
+//   console.log(k+"="+v)
+// }
